367ac670fe9050559fe63b8f48d5dc63baf511f7
victor toledo <querynetworking@gmail.com>
2023-05-16 22:25:31 -0300

    configuracion de controllers

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,14 +1,52 @@
+const TicketControl = require('../models/ticket-control');
+
+const ticketControl = new TicketControl();
 
 
 
 const socketController = (socket) => {
 
-    socket.on('enviar-mensaje', ( payload, callback ) => {
+    // Cuando un cliente se conecta
+    socket.emit( 'ultimo-ticket', ticketControl.ultimo );
+    socket.emit( 'estado-actual', ticketControl.ultimos4 );
+    socket.emit( 'tickets-pendientes', ticketControl.tickets.length);
         
-        const id = 123456789;
-        callback( id );
 
-        socket.broadcast.emit('enviar-mensaje', payload );
+    socket.on('siguiente-ticket', ( payload, callback ) => {
+        
+        const siguiente = ticketControl.siguiente();
+        callback( siguiente );
+        socket.broadcast.emit( 'tickets-pendientes', ticketControl.tickets.length);
+
+    });
+
+    socket.on('atender-ticket', ({ escritorio }, callback) => {
+        
+        if ( !escritorio ) {
+            return callback({
+                ok: false,
+                msg: 'Es escritorio es obligatorio'
+            });
+        }
+
+        const ticket = ticketControl.atenderTicket( escritorio );
+
+        
+        socket.broadcast.emit( 'estado-actual', ticketControl.ultimos4 );
+        socket.emit( 'tickets-pendientes', ticketControl.tickets.length);
+        socket.broadcast.emit( 'tickets-pendientes', ticketControl.tickets.length);
+
+        if ( !ticket ) {
+            callback({
+                ok: false,
+                msg: 'Ya no hay tickets pendientes'
+            });
+        } else {
+            callback({
+                ok: true,
+                ticket
+            })
+        }
 
     })
 
@@ -19,4 +57,3 @@ const socketController = (socket) => {
 module.exports = {
     socketController
 }
-
