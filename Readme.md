4ea13372d84dc4b167939057abca803b387ed98c
victor toledo <querynetworking@gmail.com>
2023-05-21 22:39:36 -0300

    sistema turnos demo

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Sistema de turnos Pantalla
+----------------------------
+
+# Recounstruir modulos de node
+""" nmp install """
+
+# Ejecutar
+""" nodemon app """
--- public/js/nuevo-ticket.js
+++ public/js/nuevo-ticket.js
@@ -15,7 +15,7 @@ socket.on('disconnect', () => {
 });
 
 socket.on('ultimo-ticket', (ultimo) => {
-    lblNuevoTicket.innerText = 'Ticket ' + ultimo;
+    lblNuevoTicket.innerText = 'Turno ' + ultimo;
 })
 
 
